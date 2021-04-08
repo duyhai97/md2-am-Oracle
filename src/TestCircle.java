fdd76cfccac1373edf023ecf3a977a763e8a4e70
duyhai97 <79962575+duyhai97@users.noreply.github.com>
2021-04-08 16:40:14 +0700

    aa

---
+++ src/TestCircle.java
@@ -0,0 +1,9 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle a = new Circle();
+        a.setRadius(10);
+
+        a.getArea();
+        System.out.println("area is: " + a.getArea() + " , " + a.getRadius() + " , " );
+    }
+}
