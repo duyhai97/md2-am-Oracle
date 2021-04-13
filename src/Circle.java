cd99de4ad5ca87681d8ec6e07e93af6ebe57009e
duyhai97 <79962575+duyhai97@users.noreply.github.com>
2021-04-13 20:18:30 +0700

    â

--- src/Circle.java
+++ src/Circle.java
@@ -2,7 +2,7 @@ public class Circle {
     private double radius = 1.0;
     private String color = "red";
 
-    public Circle() {
+    public Circle(double radius, String color, boolean filled) {
     }
 
     public Circle(double radius) {
--- src/TestCircle.java
+++ src/TestCircle.java
@@ -1,9 +1,9 @@
-public class TestCircle {
-    public static void main(String[] args) {
-        Circle a = new Circle();
-        a.setRadius(10);
-
-        a.getArea();
-        System.out.println("area is: " + a.getArea() + " , " + a.getRadius() + " , " );
-    }
-}
+//public class TestCircle {
+//    public static void main(String[] args) {
+//        Circle a = new Circle(radius, color, filled);
+//        a.setRadius(10);
+//
+//        a.getArea();
+//        System.out.println("area is: " + a.getArea() + " , " + a.getRadius() + " , " );
+//    }
+//}
